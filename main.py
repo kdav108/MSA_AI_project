b0995ea652d40988c1bf497099c4691bf41ba5d7
Kunj Dave <kdav0006@student.monash.edu>
2020-06-13 16:42:51 +1000

    Built a gradient boosting model to predict parkinson's and test it's ROC AUC accuracy

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import GradientBoostingClassifier  # ML model
+from sklearn.metrics import roc_auc_score
 
 # Read data into a pandas Data frame
 data = pd.read_csv('parkinsons.data')
@@ -32,8 +34,32 @@ x = scaler.fit_transform(x)
 # print(x)
 
 # Split data for testing and training
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=40)
 
+# Initiate the AI model to be used
+model = GradientBoostingClassifier(learning_rate=0.1, n_estimators=100, max_depth=4, min_samples_leaf=1, random_state=13)
+model.fit(x_train, y_train)
+
+# Calculate the model accuracy
+predicted = model.predict(x_test)
+# print(accuracy_score(y_test, predicted)) - Can remove
+print(model.score(x_test, y_test))
+
+# Get the ROC AUC curve (From the MS Learning module)
+prob = model.predict_proba(x_test)
+# print(prob)
+roc_auc_acc = round(roc_auc_score(y_test, prob[:,1]) * 100, 2)
+print('ROC_AUC_accuracy: ' + str(roc_auc_acc) + '%')
+
+
+# Function to predict whether a person is healthy or has Parkinson's disease given some voice characteristics
+def has_parkinson():
+    input = 0
+    prediction = model.predict_proba(input)
+    if prediction > 0.5:
+        return [True, prediction]
+    else:
+        return [False, prediction]
 
 
 
